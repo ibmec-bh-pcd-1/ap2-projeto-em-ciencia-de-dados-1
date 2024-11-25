c335f6d069e3ade299fa0db2dc7c2c11a8685be4
laura-furtado <202402630989@alunos.ibmec.edu.br>
2024-11-25 15:20:54 -0300

    Finalizado

--- script.js
+++ script.js
@@ -17,14 +17,39 @@ const linesData = {
   ],
 };
 
-// Função para atualizar as estações no DOM
+let linhaselecionada = document.getElementById('line-selector');
+//Função para atualizar as estações no DOM
 function updateStations(selectedLine, stationsContainer) {
-  // SEU CÓDIGO AQUI
-}
+  stationsContainer = document.getElementById("stations-container");
+  selectedLine = linhaselecionada.value; //para pegar a linha escolhida
+  let resultado = linesData[selectedLine]; //pegando qual a linha e seus objetos
+  let show = document.createElement('p');
+  show.innerHTML=resultado
+  stationsContainer.appendChild(show)
+  for(let y=0; y<3; y++){
+    let objetos = document.createElement("p");
+    objetos.innerHTML = linesData[resultado][y]; //para acessar os objetos 
+    stationsContainer.appendChild(objetos)
+    for (let i=0; i<2; i++){
+      if (linesData[resultado][i]==="normal"){
+        objetos.setAttribute("class", "normal");
+      }
+      else if (linesData[resultado][i]==="manutenção"){
+        objetos.setAttribute("class", "manutenção");
+      }
+      else if(linesData[resultado][i]==="inoperante"){
+        objetos.setAttribute("class", "inoperante");
+      }
+  }
+
+  }
+  }
+
 
 // Função principal para adicionar eventos (executada no navegador)
 function initApp() {
   // SEU CÓDIGO AQUI
+  linhaselecionada.addEventListener("change", updateStations)
 }
 
 // Garantir que a função principal seja chamada apenas no navegador
@@ -33,4 +58,4 @@ if (typeof window !== "undefined") {
 }
 
 // Exportar dados e função para testes
-module.exports = { linesData, updateStations };
+module.exports = { linesData, updateStations }
