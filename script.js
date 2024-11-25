93f11d60633af60ed7715e43e66040872d26f414
vnevesl <vineveslima11@gmail.com>
2024-11-25 15:06:14 -0300

    vitor-neves

--- script.js
+++ script.js
@@ -20,6 +20,40 @@ const linesData = {
 // Função para atualizar as estações no DOM
 function updateStations(selectedLine, stationsContainer) {
   // SEU CÓDIGO AQUI
+  let stationsContainer = document.createElement("stations-cointainer")
+  let selectedLine = document.getElementById("line-selector").value
+
+  if(selectedLine == Azul){
+    for (let i=0; i<3; i++){
+      let filho = document.createElement("p")
+      filho.innerHTML = `${linesData[0].name}`
+      stationsContainer.appendChild(filho)
+      let none = document.getElementsByTagName("p")
+      none.style.display = "none"
+    }
+  }
+  else if(selectedLine == rosa){
+    for (let i=0; i<3; i++){
+      let filho = document.createElement("p")
+      filho.innerHTML = `${linesData[1].name}`
+      stationsContainer.appendChild(filho)
+      let none = document.getElementsByTagName("p")
+      none.style.display = "none"
+    }
+  }
+  else if(selectedLine == lilas){
+    for (let i=0; i<3; i++){
+      let filho = document.createElement("p")
+      filho.innerHTML = `${linesData[2].name}`
+      stationsContainer.appendChild(filho)
+      let none = document.getElementsByTagName("p")
+      none.style.display = "none"
+    }
+  }
+  else{
+    let none = document.getElementsByTagName("p")
+    none.style.display = "black"
+  }
 }
 
 // Função principal para adicionar eventos (executada no navegador)
