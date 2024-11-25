2fcad7dece6023c2da9cbf00b18b5a7602df4e04
LucasBuccini <lucasmbuccini@gmail.com>
2024-11-25 14:47:42 -0300

    prova lucas buccini

--- script.js
+++ script.js
@@ -25,6 +25,60 @@ function updateStations(selectedLine, stationsContainer) {
 // Função principal para adicionar eventos (executada no navegador)
 function initApp() {
   // SEU CÓDIGO AQUI
+  let select = document.getElementById("line-selector")
+  let container=document.getElementById("stations-container")
+  let mensagem=document.getElementById("mensagem")
+  // Paragrafo para cada mensagem
+  let info1 = document.createElement("p")
+  let info2 = document.createElement("p")
+  let info3 = document.createElement("p")
+  //pegando todos os paragrafos para analisar o status da linha
+  let status = document.getElementsByTagName("p")
+  document.addEventListener("change",function(){
+    if(select.value=="azul"){
+      mensagem.style.display= "none"
+      info1.innerHTML="Estação Central - Normal"
+      info2.innerHTML="Estação Sul - Manutenção"
+      info3.innerHTML="Estação Norte - Inoperante"
+      //colocando a classe com base no status
+      info1.setAttribute("class","normal")
+      info2.setAttribute("class","manutenção")
+      info3.setAttribute("class","inoperante")
+      container.appendChild(info1)
+      container.appendChild(info2)
+      container.appendChild(info3)
+    }
+    else if(select.value=="rosa"){
+      mensagem.style.display= "none"
+      info1.innerHTML="Estação Jardim - Normal"
+      info2.innerHTML="Estação Flor - Normal"
+      info3.innerHTML="Estação Bela Vista - Manutenção"
+      info1.setAttribute("class","normal")
+      info2.setAttribute("class","normal")
+      info3.setAttribute("class","manutenção")
+      container.appendChild(info1)
+      container.appendChild(info2)
+      container.appendChild(info3)
+    }
+    else if(select.value=="lilas"){
+      mensagem.style.display= "none"
+      info1.innerHTML="Estação Aurora - Normal"
+      info2.innerHTML="Estação Cristal - Inoperante"
+      info3.innerHTML="Estação Pérola - Manutenção"
+      info1.setAttribute("class","normal")
+      info2.setAttribute("class","inoperante")
+      info3.setAttribute("class","manutenção")
+      container.appendChild(info1)
+      container.appendChild(info2)
+      container.appendChild(info3)
+    }else{
+      mensagem.style.display="block"
+      container.removeChild(info1)
+      container.removeChild(info2)
+      container.removeChild(info3)
+      //removendo as mensagens adicionadas previamente
+    }
+  })
 }
 
 // Garantir que a função principal seja chamada apenas no navegador
