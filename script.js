e458abe544b2382202720a4d6cd1e2e9f025becf
Mateus Milanez <mateusmilanezdepaula@hotmail.com>
2024-11-25 15:20:25 -0300

    finalizado

--- script.js
+++ script.js
@@ -16,15 +16,36 @@ const linesData = {
     { name: "Estação Pérola", status: "Manutenção" },
   ],
 };
-
+let linhaatual = document.getElementById("line-selector");
+let valorlinha = linhaatual.value;
+let paragrafo = document.getElementsByTagName("p");
+let stationsContainer2 = document.getElementById("stations-container");
+let novo = document.getElementsByTagName("p")
 // Função para atualizar as estações no DOM
 function updateStations(selectedLine, stationsContainer) {
-  // SEU CÓDIGO AQUI
-}
+  if (selectedLine==="azul"){
+    paragrafo.innerHTML(`${linesData[0]}`)
+    stationsContainer.appendchild(paragrafo) 
+  };
+  if (selectedLine==="rosa"){
+    paragrafo.innerHTML(`${linesData[1]}`)
+    stationsContainer.appendchild(newe2) 
+  };
+  if (selectedLine==="lilas"){
+    paragrafo.innerHTML(`${linesData[2]}`)
+    stationsContainer.appendchild(newe2) 
+  };
+  if (selectedLine===""){
+    paragrafo.innerHTML(`Selecione uma linha para visualizar as estações`)
+    stationsContainer.appendchild(paragrafo)
+
+}};
 
 // Função principal para adicionar eventos (executada no navegador)
-function initApp() {
-  // SEU CÓDIGO AQUI
+seletor = document.getElementById("line-selector");
+function initApp(event) {
+  event.defaultPrevented
+  seletor.addEventListener("change",updateStations(valorlinha,stationsContainer2))
 }
 
 // Garantir que a função principal seja chamada apenas no navegador
@@ -32,5 +53,3 @@ if (typeof window !== "undefined") {
   document.addEventListener("DOMContentLoaded", initApp);
 }
 
-// Exportar dados e função para testes
-module.exports = { linesData, updateStations };
