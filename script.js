66e2fd7072dd6e4317da84dd5c7e432cb489cf4c
Joao Castro <jpcastrocruzeirotetra2006@gmail.com>
2024-11-25 15:05:27 -0300

    ap2 joao pedro castro

--- script.js
+++ script.js
@@ -17,14 +17,49 @@ const linesData = {
   ],
 };
 
+
 // Função para atualizar as estações no DOM
 function updateStations(selectedLine, stationsContainer) {
-  // SEU CÓDIGO AQUI
+  let stationsContainer = document.getElementById("stations-container")
+  let selectedLine = document.getElementById("line-selector").value
+  if(selectedLine == "azul"){
+    for (let i = 0; i<3; i++){
+      let filho = document.createElement("p")
+      filho.innerHTML = `${linesData[0][i]}`
+      stationsContainer.appendchild(filho)
+      let texto = document.getElementsByTagName("p")
+      texto.style.display = "none"
+    }
+  }
+  else if(selectedLine == "rosa"){
+    for (let i = 0; i<3; i++){
+      let filho = document.createElement("p")
+      filho.innerHTML = `${linesData[1][i]}`
+      stationsContainer.appendchild(filho)
+      let texto = document.getElementsByTagName("p")
+      texto.style.display = "none"
+    }
+  }
+  else if(selectedLine == "lilas"){
+    for (let i = 0; i<3; i++){
+      let filho = document.createElement("p")
+      filho.innerHTML = `${linesData[2][i]}`
+      stationsContainer.appendchild(filho)
+      let texto = document.getElementsByTagName("p")
+      texto.style.display = "none"
+    }
+  }
+  else{
+    let texto = document.getElementsByTagName("p")
+    texto.style.display = "block"
+  }
 }
 
 // Função principal para adicionar eventos (executada no navegador)
 function initApp() {
-  // SEU CÓDIGO AQUI
+  updateStations()
+  
+
 }
 
 // Garantir que a função principal seja chamada apenas no navegador
